022020e5f24ceb83834e701aa8697a35a6c1602e
ArtemShadrin <artemshadrinprogramming@gmail.com>
2023-04-09 19:08:07 +0700

    Initial commit

---
+++ ads/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from users.models import User
+
+
+class Ad(models.Model):
+    name = models.CharField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    is_published = models.BooleanField()
+    price = models.PositiveIntegerField()
+    image = models.ImageField(upload_to="ad_images", null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Объявление"
+        verbose_name_plural = "Объявления"
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
